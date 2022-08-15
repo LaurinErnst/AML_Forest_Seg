2671e7a2cf39e228f1278de0dd7bcc32e465156c
ljernst <ljernst@t-online.de>
2022-08-15 14:54:15 +0200

    bilder umbenannt

---
+++ datarenamer.py
@@ -0,0 +1,13 @@
+import os
+
+# assign directory
+directory = 'data/images'
+i = 0
+
+
+for filename in os.listdir(directory):
+    f = os.path.join(directory, filename)
+    # checking if it is a file
+    if os.path.isfile(f):
+        i += 1
+        os.rename(f, directory + '/'+ str(i) + ".jpg")
