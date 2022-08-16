2ccf0a87b74686198beb83e1eb9fb03382d1ee55
ljernst <ljernst@t-online.de>
2022-08-16 12:51:59 +0200

    batchloader fertig geschrieben

--- batchloader.py
+++ batchloader.py
@@ -1,43 +1,45 @@
-from re import T
-from sys import displayhook
-import pandas as pd
 import os
 from PIL import Image
 import numpy as np
-from UNet import UNet
 import torch
 
 def batchloader(batchsize = None, batch = None):
     if batchsize == None and batch != None:
         directory = 'data/images/'
-
+        j = 0
         for i in batch:
+            
             f = directory + str(i) + ".jpg"
             im = Image.open(f)
-            data = np.array(im.getdata()).T
+            im_data = np.array(im.getdata()).T
 
-            # r = data[:,0]
-            # r = r.reshape(256, 256).astype(np.uint8)
+            if j == 0:
+                data = np.array([im_data.reshape(3, 256, 256)])
+                j += 1
+            else:
+                im_data = im_data.reshape(1, 3, 256, 256)
+                data = np.concatenate((data, im_data))
+        return torch.tensor(data)
 
-            # g = data[:,1]
-            # g = g.reshape(256, 256).astype(np.uint8)
+    if batchsize != None and batch == None:
 
-            # b = data[:,2]
-            # b = b.reshape(256, 256).astype(np.uint8)
+        batch = np.random.randint(5108, size = batchsize)
+        print(batch)
+        directory = 'data/images/'
+        j = 0
+        for i in batch:
             
-            # im = Image.fromarray(r)
-            # im.save("r.jpeg")
-            # im = Image.fromarray(g)
-            # im.save("g.jpeg")
-            # im = Image.fromarray(b)
-            # im.save("b.jpeg")
-            # print(r)
-            
-            return data
+            f = directory + str(i) + ".jpg"
+            im = Image.open(f)
+            im_data = np.array(im.getdata()).T
 
+            if j == 0:
+                data = np.array([im_data.reshape(3, 256, 256)])
+                j += 1
+            else:
+                im_data = im_data.reshape(1, 3, 256, 256)
+                data = np.concatenate((data, im_data))
+        return torch.tensor(data)
 
-data = batchloader(None, [1])
-data = torch.tensor(data)
-
-Test = UNet()
-print(Test.forward(data))
+    if batchsize == None and batch == None:
+        raise Exception("Weder batchsize noch batch deklariert!")
