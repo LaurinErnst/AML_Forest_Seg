e9e2b5504e55736c281d29f1aa55c1e059d6a61e
Pht27 <phili2709@gmail.com>
2022-08-27 14:04:09 +0200

    net saver implemented

---
+++ src/data_handling/net_saver.py
@@ -0,0 +1,12 @@
+import pickle
+
+
+def save_model(model, name, model_type):
+	with open("trained_models/" + model_type + name, "wb") as file:
+		pickle.dump(model, file)
+
+
+def load_model(name, model_type):
+	with open("trained_models/" + model_type + name, "rb") as file:
+		model = pickle.load(file)
+	return model
