80bf8a9c2053e191e2e5f6377fc7d26e8a8f2945
LaurinErnst <ljernst@t-online.de>
2022-09-05 12:29:51 +0200

    gpu wird gedruckt

--- src/config/gen_config.py
+++ src/config/gen_config.py
@@ -9,5 +9,7 @@ TRAIN_SIZE = 4000
 
 # determine the device to be used for training and evaluation
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+print("Device: " + DEVICE)
 # determine if we will be pinning memory during data loading
 PIN_MEMORY = True if DEVICE == "cuda" else False
