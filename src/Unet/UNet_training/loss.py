c3b6bb212ecf98225466f3134ee197606609dee7
ljernst <ljernst@t-online.de>
2022-09-10 10:53:15 +0200

    aufgeräumt

--- src/Unet/UNet_training/loss.py
+++
@@ -1,6 +0,0 @@
-import torch
-
-
-# VERALTET WIR NUTZEN ZUR ZEIT PYTORCH LOSS NOCH DA FALL WIR CUSTOM LOSS WOLLEN
-def mean_square_loss(y_gen, y_real):
-    diff = torch.square(torch.sub(y_gen, y_real))
