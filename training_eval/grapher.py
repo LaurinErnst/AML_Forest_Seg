3bfb0e78a20e6addd98cb4b4b823e228863da168
ljernst <ljernst@t-online.de>
2022-09-13 13:17:35 +0200

    ergebnis auswertung

---
+++ training_eval/grapher.py
@@ -0,0 +1,86 @@
+from inspect import formatannotationrelativeto
+import matplotlib.pyplot as plt
+import pickle
+import os
+import torch
+import numpy as np
+import pandas as pd
+import csv
+
+# assign directo
+directory = input("Directory containing results: ")
+
+convert = ""
+while convert != "y" and convert != "n":
+    convert = input("convert pickles to csv?(needed if not done already) (y/n): ")
+
+# convert pickles to csv
+if convert == "y":
+
+    # iterate over files in
+    # that directory
+    for filename in os.listdir(directory + "/loss_graphs"):
+        f = os.path.join(directory + "/loss_graphs", filename)
+        # checking if it is a file
+        if os.path.isfile(f):
+            with open(f, "rb") as file:
+                data = pickle.load(file)
+            if not os.path.exists(directory + "/csv_data"):
+                os.mkdir(directory + "/csv_data")
+            o = os.path.join(directory + "/csv_data", filename + "_data.csv")
+
+            # saving data in csv
+            data_np = np.array(data)  # convert to Numpy array
+            df = pd.DataFrame(data_np)  # convert to a dataframe
+            df.to_csv(o, index=False)  # save to file
+
+if not os.path.exists(directory + "/graphs"):
+    os.mkdir(directory + "/graphs")
+
+file_names = pd.DataFrame
+file_names_file = input("path/to/file_name_file: ")
+
+with open("training_eval/" + file_names_file, mode="r") as infile:
+    file_names = csv.reader(infile)
+    i = 0
+    data = np.zeros([2, 15])
+    for file_name in file_names:
+        print("Name of model: {}".format(file_name))
+
+        with open(directory + "/csv_data/" + file_name[0], mode="r") as infile:
+            dat = []
+            for row in csv.reader(infile):
+                if row[0] != "0":
+
+                    dat.append(float(row[0]))
+            data[i] = dat
+        if i == 1:
+
+            # Initialise the figure and axes.
+            fig, ax = plt.subplots(1, figsize=(8, 6))
+
+            # Set the title for the figure
+            fig.suptitle(input("title of plot: "), fontsize=15)
+            plt.xlabel(input("x_label: "))
+            plt.ylabel(input("y_label: "))
+            np.flip(data, axis=1)
+            # Draw all the lines in the same plot, assigning a label for each one to be
+            # shown in the legend.
+            ax.plot(range(1, 16), data[0], label="training_loss")
+            ax.plot(range(1, 16), data[1], label="test_loss")
+            plt.ylim(min([min(data[0]), min(data[1])]), 1)
+            # Add a legend, and position it on the lower right (with no box)
+            plt.legend(loc="upper right", frameon=False)
+            plt.show()
+
+            save = ""
+
+            while save != "y" and save != "n":
+                save = input("save plot? (y/n) ")
+
+            if save == "y":
+                name = input("name of file: ")
+                plt.savefig(directory + "/graphs/" + name + ".png")
+            i = 0
+        elif i == 0:
+            i += 1
