4e0854746cf435f9215bf53bc3a430246447c8f7
unknown <brudernico@gmail.com>
2022-09-22 12:37:49 +0200

    jaccard_index und naive_seg aber naive_seg noch nicht auf package angpasst

--- training_eval/losses.py
+++ training_eval/losses.py
@@ -9,3 +9,17 @@ def percentage_loss(x, y):
 	y = np.round(y)
 
 	return np.average(x == y)
+
+def jaccard_index(mask_true,mask_pred):
+
+    n_true = np.count_nonzero(mask_true==1)
+    n_pred = np.count_nonzero(mask_pred==1)
+
+    forest_true = mask_true==1
+    forest_pred = mask_pred==1
+
+    bool_array = forest_true*forest_pred
+    n_cap = np.count_nonzero(bool_array)
+
+    jaccard_index=n_cap/(n_true+n_pred-n_cap)
+    return(jaccard_index)
