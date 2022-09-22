97e210dddf3348a7ac3dacf50239ea419bec3b29
Pht27 <phili2709@gmail.com>
2022-09-22 14:23:42 +0200

    mask overlay

--- training_eval/losses.py
+++ training_eval/losses.py
@@ -1,31 +1,54 @@
 import numpy as np
-from data_handling.dataloader import  *
+from data_handling.dataloader import dataloader
+import pickle
+import torch
+import io
 
 
 def percentage_right(x, y):
-	x = np.flatten(x)
-	y = np.flatten(y)
+    x = x.flatten()
+    y = y.flatten()
 
-	x = np.round(x)
-	y = np.round(y)
+    x = torch.round(x)
+    y = torch.round(y)
 
-	return np.average(x == y)
+    print(x)
+    print(y)
+    print(np.average(x == y))
+    return np.average(x == y)
 
 
 def test_model(model, n=5):
-	dl = dataloader.dataloader(0, 0, 0)
-	X, Y = dl.load_all()
+    dl = dataloader(0, 0, 0)
 
-	losses = []
+    model.eval()
 
-	for i, x in enumerate(X):
-		y = Y[i]
-		y_hat = model(x)
+    losses = []
 
-		losses.append(percentage_right(y, y_hat))
+    for i in range(5108):
+        x, y = dl.load_all()
+        y = y / 255
 
-	sorted_indices = np.argsort(losses)
-	worst = sorted_indices[:n]
-	best = np.flip(sorted_indices[-n:])
-	return losses, np.average(losses), worst, best
+        if i % 100 == 0:
+            print(i/5108)
+        y_hat = model(x)
 
+        losses.append(percentage_right(y, y_hat))
+
+    sorted_indices = np.argsort(losses)
+    worst = sorted_indices[:n]
+    best = np.flip(sorted_indices[-n:])
+    return losses, np.average(losses), worst, best
+
+
+class NetRecovery(pickle.Unpickler):
+    def find_class(self, module, name):
+        if module == 'torch.storage' and name == '_load_from_bytes':
+            return lambda b: torch.load(io.BytesIO(b), map_location='cpu')
+        else:
+            return super().find_class(module, name)
+
+
+with open("testmodel", "rb") as file:
+    m = NetRecovery(file).load()
+print(test_model(m))
---
+++ training_eval/mask_comparison.py
@@ -0,0 +1,61 @@
+import pickle
+import torch
+import io
+
+import torchvision.transforms as t
+from data_handling.dataloader import dataloader
+
+
+class NetRecovery(pickle.Unpickler):
+	def find_class(self, module, name):
+		if module == 'torch.storage' and name == '_load_from_bytes':
+			return lambda b: torch.load(io.BytesIO(b), map_location='cpu')
+		else:
+			return super().find_class(module, name)
+
+
+def convert_mask_to_transparent(mask):
+	pixels = mask.load()
+	for i in range(mask.size[0]):
+		for j in range(mask.size[1]):
+			if pixels[i, j][0] == 0:
+				pixels[i, j] = (0, 0, 0, 0)
+			else:
+				pixels[i, j] = (212, 49, 49, 80)
+	return mask
+
+
+def create_overlap(background, overlap):
+	transform = t.ToPILImage()
+
+	background = transform(background / 255).convert("RGBA")
+	overlap = transform(overlap / 255).convert("RGBA")
+	overlap = convert_mask_to_transparent(overlap)
+
+	background.paste(overlap, (0, 0), overlap)
+	background.show()
+
+	return background
+
+
+def create_masks(model, indices):
+	dl = dataloader(0, 0, 0)
+	images = []
+
+	for i in indices:
+		img, mask = dl.load_one(i)
+		y = model(img)
+
+		real_overlap = create_overlap(img[0], mask[0])
+		calc_overlap = create_overlap(img[0], y[0])
+
+		images.append((i, real_overlap, calc_overlap))
+
+	return images
+
+
+with open("testmodel", "rb") as file:
+	m = NetRecovery(file).load()
+create_masks(m, [1, 2, 3, 4])
+
+
