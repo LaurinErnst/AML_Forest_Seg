c155e84cff5e897842022cb4ae87b3be08332a6f
Pht27 <phili2709@gmail.com>
2022-09-22 14:44:07 +0200

    syntax

--- training_eval/losses.py
+++ training_eval/losses.py
@@ -2,24 +2,23 @@ import numpy as np
 
 
 def percentage_loss(x, y):
-	x = np.flatten(x)
-	y = np.flatten(y)
+    x = np.flatten(x)
+    y = np.flatten(y)
 
-	x = np.round(x)
-	y = np.round(y)
+    x = np.round(x)
+    y = np.round(y)
 
-	return np.average(x == y)
+    return np.average(x == y)
 
-def jaccard_index(mask_true,mask_pred):
 
-    n_true = np.count_nonzero(mask_true==1)
-    n_pred = np.count_nonzero(mask_pred==1)
+def jaccard_index(mask_true, mask_pred):
+    n_true = np.count_nonzero(mask_true == 1)
+    n_pred = np.count_nonzero(mask_pred == 1)
 
-    forest_true = mask_true==1
-    forest_pred = mask_pred==1
+    forest_true = mask_true == 1
+    forest_pred = mask_pred == 1
 
-    bool_array = forest_true*forest_pred
+    bool_array = forest_true * forest_pred
     n_cap = np.count_nonzero(bool_array)
 
-    jaccard_index=n_cap/(n_true+n_pred-n_cap)
-    return(jaccard_index)
+    return n_cap / (n_true + n_pred - n_cap)
